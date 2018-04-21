7b69311f3a7b8945a08e7a54e27ab0d81fcca256
rauni09 <36557264+rauni09@users.noreply.github.com>
2018-04-21 10:46:22 +0530

    Update Set4-q1.py

--- SET 4/Set4-q1.py
+++ SET 4/Set4-q1.py
@@ -1,30 +1,6 @@
 
 from math import pow as p
 binaryNumber=input("\nEnter a binary number : ")
- 
-#FOR CONFIRMING THAT USER's INPUT IS A BINARY NUMBER ONLY
-flag=0                
-while flag==0:
-  temp_bin=binaryNumber
-  if len(binaryNumber)==1:
-    if binaryNumber=='0' or binaryNumber=='1':
-      flag=1
-    else:
-      binaryNumber=input("Please input a valid binary number - ")
-      temp_bin=binaryNumber
-      flag=0
-  else:
-    init=0
-    while init!=len(temp_bin)-1:
-      init+=1
-      if temp_bin[init]=='0' or temp_bin[init]=='1':
-        flag=1 
-      else:
-        print("Error!The number inputted is not a binary number!! Kindly enter only 0's and 1's!")
-        binaryNumber=input("\nEnter the binary number again : ")
-        temp_bin=binaryNumber
-        init=0
-        flag=0
 
 #CONVERTING BIN TO OCT      
 binaryNumber=int(binaryNumber)
